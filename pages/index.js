c85eea97ad3e0eac86a9d38e8b0b8029004460e9
AnthonyOkwananke <57677240+AnthonyOkwananke@users.noreply.github.com>
2021-02-21 11:12:09 -0700

    my ecommerce shop

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,68 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+import products from '../products.json';
+
 export default function Home() {
+  console.log('products', products);
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>My Shop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          My Shop
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          The best shop on the web
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <ul className={styles.grid}>
+          {products.map(product => {
+            const { id, title, price, description, image } = product;
+            return (
+              <li key={ id } className={styles.card}>
+                <a href="#" >
+                  <img src={ image } alt={ title } />
+                  <h3>{ title }</h3>
+                  <p>${ price }</p>
+                  <p>{ description }</p>
+                </a>
+              </li>
+            )
+          })}
+          {/* <li className={styles.card}>
+            <a href="https://nextjs.org/docs" >
+              <img src="/images/rv3.jpg" alt="modern-rv" />
+              <h3>State-of-the-Art</h3>
+              <p>$10,000.00</p>
+              <p>Bring Cosmo the space Jellyfish to your wardrobe with this hugh quality tshirt</p>
+            </a>
+          </li>
+          
+          <li className={styles.card}>
+            <a href="https://nextjs.org/learn" >
+              <img src="/images/rv2.jpg" alt="antique-rv" />
+              <h3>Blast from the Past</h3>
+              <p>$20,000.00</p>
+              <p>Add some flare to your life with a sticcker of the Cosmo JellyFish</p>
+            </a>
+          </li>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <li className={styles.card}>
+            <a href="https://github.com/vercel/next.js/tree/master/examples">
+              <img src="/images/teslarv.jpg" alt="future-rv" />
+              <h3>The Future</h3>
+              <p>$40,000.00</p>
+              <p>Show your love for Cosmo with a tshirt and sticker combo pack</p>
+            </a>
+          </li> */}
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
+          {/* <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
@@ -46,8 +70,8 @@ export default function Home() {
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
-          </a>
-        </div>
+          </a> */}
+        </ul>
       </main>
 
       <footer className={styles.footer}>
